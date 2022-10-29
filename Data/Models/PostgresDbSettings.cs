695623cba8d6efaf3ba56c281892f8c6ba6db827
Bashiruu1 <silvergold21@gmail.com>
2022-10-28 21:55:19 -0400

    resolves, updated time on entities.. controller work

--- Data/Models/PostgresDbSettings.cs
+++ Data/Models/PostgresDbSettings.cs
@@ -1,17 +1,17 @@
 namespace EFCoreDemo.Data.Models;
 
 public class PostgresDbSettings
-    {
-        public string? Host { get; set; }
-        public string? DbName { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }
+{
+    public string? Host { get; set; }
+    public string? DbName { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
 
-        public string ConnectionString 
-        { 
-            get
-            {
-                return $"Host={Host};Database={DbName};Username={Username};Password={Password}";
-            }
+    public string ConnectionString 
+    { 
+        get
+        {
+            return $"Host={Host};Database={DbName};Username={Username};Password={Password}";
         }
     }
+}
